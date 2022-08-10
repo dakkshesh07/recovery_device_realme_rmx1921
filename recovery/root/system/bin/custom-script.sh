064fd891d978d44a375782086b74c81f2da0c0a3
pjgowtham <pjgowtham@gmail.com>
2022-08-10 22:10:37 +0530

    nicky: Add methods to detect if we are using latest firmware for use by flashable zips

--- recovery/root/system/bin/custom-script.sh
+++ recovery/root/system/bin/custom-script.sh
@@ -6,6 +6,14 @@ FP_ID=$(cat /proc/fp_id)
 # ro.build.product overrides to support non-unified custom ROMs
 # ro.product.product.device overrides to support realme ui 1 flash
 
+if grep -q androidboot.prjname /proc/cmdline; then
+    echo "custom-script: Detected realme UI 2 firmware" > /tmp/recovery.log
+    resetprop ro.device.latest_fw true
+else
+    echo "custom-script: Detected realme UI 1 firmware" > /tmp/recovery.log
+    resetprop ro.device.latest_fw false
+fi
+
 if grep -q SDM710 /proc/cpuinfo && [ $FP_ID = F_1511 ]; then
   setprop ro.boot.prjname 18621
   resetprop ro.build.product RMX1851
